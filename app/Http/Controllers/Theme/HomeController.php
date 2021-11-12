3f6530e4576ed140ba3966245f9639f9418c7615
unknown <AntonisMoul>
2021-11-12 13:49:18 +0200

    push

--- app/Http/Controllers/Theme/HomeController.php
+++ app/Http/Controllers/Theme/HomeController.php
@@ -613,7 +613,16 @@ class HomeController extends Controller
         }
         $categoryScript = $event->category->first() ? 'Event > ' . $event->category->first()->name : '';
         
-        $data['tigran'] = ['Price' => $price,'Product_id' => $event->id,'Product_SKU' => $event->id,'ProductCategory' => $categoryScript, 'ProductName' =>  $event->title,'Event_ID' => 'kc_' . time() ];
+        $tr_price = $price;
+        if($tr_price - floor($tr_price)>0){
+            $tr_price = number_format($tr_price , 2 , '.', ',');
+        }else{
+            $tr_price = number_format($tr_price , 0 , '.', '');
+            $tr_price = strval($tr_price);
+            $tr_price.".00";
+        }
+
+        $data['tigran'] = ['Price' => $tr_price,'Product_id' => $event->id,'Product_SKU' => $event->id,'ProductCategory' => $categoryScript, 'ProductName' =>  $event->title,'Event_ID' => 'kc_' . time() ];
 
         if(Auth::user() && count(Auth::user()->events->where('id',$event->id)) > 0){
             $data['is_event_paid'] = 1;
