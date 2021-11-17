882290042f23bc1fd32593b580626c187f100633
unknown <AntonisMoul>
2021-11-17 17:02:04 +0200

    push

--- app/Http/Controllers/Theme/InfoController.php
+++ app/Http/Controllers/Theme/InfoController.php
@@ -176,17 +176,17 @@ class InfoController extends Controller
         $this->fbp->sendPurchaseEvent($data);
 
         //DESTROY CART HERE AND SESSION vars
-        //Cart::instance('default')->destroy();
-        //Session::forget('pay_seats_data');
-        //Session::forget('transaction_id');
-        //Session::forget('cardtype');
-        //Session::forget('installments');
-        ////Session::forget('pay_invoice_data');
-        //Session::forget('pay_bill_data');
-        //Session::forget('deree_user_data');
-        //Session::forget('user_id');
-        //Session::forget('coupon_code');
-        //Session::forget('coupon_price');
+        Cart::instance('default')->destroy();
+        Session::forget('pay_seats_data');
+        Session::forget('transaction_id');
+        Session::forget('cardtype');
+        Session::forget('installments');
+        //Session::forget('pay_invoice_data');
+        Session::forget('pay_bill_data');
+        Session::forget('deree_user_data');
+        Session::forget('user_id');
+        Session::forget('coupon_code');
+        Session::forget('coupon_price');
         
         ///dd($data);
         
