175ec4870af1a1d47153a773c690920396e74d02
Anto Cabraja <anto@qedcode.io>
2021-12-20 00:16:04 +0100

    [KC-5] Added basic sentry integration

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -188,6 +188,18 @@
       0 => 'Propaganistas\\LaravelPhone\\PhoneServiceProvider',
     ),
   ),
+  'sentry/sentry-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Sentry\\Laravel\\ServiceProvider',
+      1 => 'Sentry\\Laravel\\Tracing\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Sentry' => 'Sentry\\Laravel\\Facade',
+    ),
+  ),
   'vemcogroup/laravel-sparkpost-driver' => 
   array (
     'providers' => 
