c828b658b1b14e2b0c515849442d33793a38efd5
Josip Peroš <josip@qedcode.net>
2022-01-04 15:47:57 +0100

    [KC-1] added ubold resuorces

---
+++ resources/admin/resources/js/pages/tickets.js
@@ -0,0 +1,26 @@
+/*
+Template Name: Ubold - Responsive Bootstrap 4 Admin Dashboard
+Author: CoderThemes
+Website: https://coderthemes.com/
+Contact: support@coderthemes.com
+File: Tickets init js
+*/
+
+$(document).ready(function () {
+    $('#tickets-table').DataTable({
+        "language": {
+            "paginate": {
+                "previous": "<i class='mdi mdi-chevron-left'>",
+                "next": "<i class='mdi mdi-chevron-right'>"
+            }
+        },
+        "drawCallback": function () {
+            $('.dataTables_paginate > .pagination').addClass('pagination-rounded');
+        }
+    });
+
+    $(".dataTables_length select").addClass('form-select form-select-sm');
+    $(".dataTables_length select").removeClass('custom-select custom-select-sm');
+
+    $(".dataTables_length label").addClass('form-label');
+});
