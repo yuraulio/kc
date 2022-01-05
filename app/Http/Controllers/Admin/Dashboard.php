46004508de145008317611d9c5750198ceb2e0dd
Josip Peroš <josip@qedcode.net>
2022-01-05 15:05:01 +0100

    [KC-1] removed auth from dashboard controller

--- app/Http/Controllers/Admin/Dashboard.php
+++ app/Http/Controllers/Admin/Dashboard.php
@@ -7,15 +7,7 @@ use Illuminate\Http\Request;
 
 class Dashboard extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+
 
     /**
      * Show the application dashboard.
