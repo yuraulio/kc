703bbb0ae9cb8ce48532995900fc663f592c926a
Anto Cabraja <anto@qedcode.io>
2022-01-15 17:56:43 +0100

    [KC-6] Add separate variable for APP_DOMAIN

--- routes/admin.php
+++ routes/admin.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Admin\Dashboard;
 use App\Http\Controllers\Auth\AdminLoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::domain('admin.' . env('APP_URL'))->group(function () {
+Route::domain('admin.' . env('APP_DOMAIN'))->group(function () {
     Route::get('/login', [AdminLoginController::class, 'showLoginPage'])->name("admin-login");
     Route::post('/authenticate', [AdminLoginController::class, 'authenticate'])->name("admin-authenticate");
 
