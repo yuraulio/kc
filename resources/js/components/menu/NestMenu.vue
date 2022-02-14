19ab733b879b21a42c578a6de56d5976f3a3489b
Josip Peroš <josip@qedcode.net>
2022-02-14 12:45:54 +0100

    [KC-15] fixed swalert type warning

--- resources/js/components/menu/NestMenu.vue
+++ resources/js/components/menu/NestMenu.vue
@@ -177,7 +177,7 @@
                 Swal.fire({
                     title: 'Are you sure?',
                     text: 'You will not be able to recover this menu item',
-                    type: 'warning',
+                    icon: 'warning',
                     showCancelButton: true,
                     confirmButtonText: 'Yes, delete it!',
                     cancelButtonText: 'No, keep it'
