4cd03176fc8bba7b293b42909c4e3b4f530e1a90
Josip Peroš <josip@qedcode.net>
2022-02-16 14:15:52 +0100

    [KC-80] updated social share blade template

--- resources/views/global/social.blade.php
+++ resources/views/global/social.blade.php
@@ -1,7 +1,6 @@
 <div class="social-share">
     <ul class="clearfix">
-        <a target="_blank" title="Share on facebook" href="http://www.facebook.com/sharer.php?u={{ Request::url() }}" onclick="javascript:window.open(this.href,
-            '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=300');return false;">
+        <a target="_blank" title="Share on facebook" href="http://www.facebook.com/sharer.php?u={{ Request::url() }}" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=300');return false;">
             <img class="replace-with-svg" src="{{cdn('/theme/assets/images/icons/social/events/Facebook.svg')}}" width="23" alt="Share on facebook"></a></li>
 
         <a target="_blank" title="Share on Twitter" href="http://twitter.com/share?text={{ $title }}&amp;url={{ Request::url() }}&amp;via=Knowcrunch" onclick="javascript:window.open(this.href,
