fc1ebb31eb9c59d12653f236495ed1647e49c44b
Josip Peroš <josip@qedcode.net>
2022-03-08 14:08:53 +0100

    [KC-BUGFIX] testemonials component

--- resources/views/new_web/components/course_testimonials_component.blade.php
+++ resources/views/new_web/components/course_testimonials_component.blade.php
@@ -11,47 +11,51 @@
 @endphp
 
 @if(count($testimonials) > 0)
-<?php
+    <?php
         $title = '';
         $body = '';
-    if(isset($sections['testimonials'])){
-        $title = $sections['testimonials']->first()->title;
-        $body = $sections['testimonials']->first()->description;
-    }
-
+        if(isset($sections['testimonials'])){
+            $title = $sections['testimonials']->first()->title;
+            $body = $sections['testimonials']->first()->description;
+        }
     ?>
 
-        <div class="course-full-text full-w-pad">
-        <h2 class="text-align-center text-xs-left tab-title">{!!$title!!}</h2>
-        <h3 class="text-align-center text-xs-left tab-title"> {!!$body!!} </h3>
-        <div class="testimonial-carousel-wrapper hidden-xs">
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <div class="course-full-text full-w-pad">
+                <h2 class="text-align-center text-xs-left tab-title">{!!$title!!}</h2>
+                <h3 class="text-align-center text-xs-left tab-title"> {!!$body!!} </h3>
+                <div class="testimonial-carousel-wrapper hidden-xs">
 
-            <div class="video-carousel-big owl-carousel">
-                @foreach($testimonials as $key => $video)
-                <?php
+                    <div class="video-carousel-big owl-carousel">
+                        @foreach($testimonials as $key => $video)
+                        <?php
 
-                    if(!$video['video_url']){
-                    continue;
-                    }
-                    $urlArr = explode("/",$video['video_url']);
-                    $urlArrNum = count($urlArr);
+                            if(!$video['video_url']){
+                            continue;
+                            }
+                            $urlArr = explode("/",$video['video_url']);
+                            $urlArrNum = count($urlArr);
 
-                    // YouTube video ID
-                    $youtubeVideoId = $urlArr[$urlArrNum - 1];
+                            // YouTube video ID
+                            $youtubeVideoId = $urlArr[$urlArrNum - 1];
 
-                    // Generate youtube thumbnail url
-                    $thumbURL = 'https://img.youtube.com/vi/'.$youtubeVideoId.'/mqdefault.jpg';
-                    ?>
-                <div class="slide">
-                    <a data-fancybox href="{{ $video['video_url'] }}"><img src="{{ $thumbURL }}" alt=""/></a>
+                            // Generate youtube thumbnail url
+                            $thumbURL = 'https://img.youtube.com/vi/'.$youtubeVideoId.'/mqdefault.jpg';
+                            ?>
+                        <div class="slide">
+                            <a data-fancybox href="{{ $video['video_url'] }}"><img src="{{ $thumbURL }}" alt=""/></a>
+                        </div>
+                        @endforeach
+                    </div>
+                    <!-- /.testimonial-carousel-wrapper -->
                 </div>
-                @endforeach
+                <!-- /.course-full-text -->
+                </div>
             </div>
-            <!-- /.testimonial-carousel-wrapper -->
-        </div>
-        <!-- /.course-full-text -->
         </div>
     </div>
+
     <div class="user-testimonial-wrapper">
         <div class="container">
         <div class="user-testimonial-big owl-carousel">
