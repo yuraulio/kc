f6b2c32b7ada8e756a681a3c227063711f3c60a3
Anto Cabraja <anto@qedcode.io>
2022-03-24 12:57:56 +0100

    [KC] Update view image modal

--- resources/js/components/media/gallery.vue
+++ resources/js/components/media/gallery.vue
@@ -1,98 +1,84 @@
 <template>
   <div class="card-body" >
-    <h4 class="header-title">Subfiles
-        <button @click="confirmSelection" v-if="selectedImages.length" class="float-end btn btn-soft-success btn-rounded"> Confirm Selection</button>
+    <h4 class="header-title">
+        <button @click="confirmSelection" v-if="selectedImages.length" class="float-end btn btn-soft-success btn-rounded">
+            Confirm Selection
+        </button>
     </h4>
-    <div class="row">
-      <div  v-for="im in activeImg ? lodash.find(images, {id: activeImg}).subfiles : []" class="col-sm-2" style="cursor: pointer">
-            <img @click="selectImg(im)" :src="im.url" alt="image" class="img-fluid rounded" width="200" :style="'height: 100px; width: auto; ' + (im.subselected ? '    border: 4px solid #1abc9c;' : '')">
-            <p class="mb-0">
 
-                <code>
-                    <template v-if="im.name.length < 20">
-                        {{ im.name }}
+    <div class="row">
+        <div class="col-9">
+            <div style="height: 600px" id="" class="carousel">
+                <div  class="carousel-inner" role="listbox">
+                    <div v-for="(img, index) in selectedImage ? [selectedImage] : images" :class="'carousel-item ' + (activeImg == img.id ? ' active' : '')" :key="index">
+                    <template v-if="!lodash.find(img.subfiles, {'subselected' : true})">
+                        <div v-if="img.extension == 'pdf'" style="width: 100%; height: 300px" class=" text-secondary rounded text-center">
+                            <i class="mdi mdi-file-pdf-outline font-28"></i>
+                        </div>
+                        <img v-else :src="img.url" alt="..." class="d-block img-fluid" />
+                        <div class="carousel-caption d-none d-md-block card bg-dark" style="margin-bottom: -60px">
+                            <h3 class="text-white">
+                                {{ img.name.length < 40 ? img.name : limit(img.name, 40) + '...' }}
+                                <span @click="$set(img, 'selected', false)" v-if="img.selected" class="badge bg-success rounded-pill float-end pr-2" style="line-height: 1.1;margin-right: 5px; margin-top: -25px;cursor: pointer">
+                                    <i class="mdi mdi-check"></i>
+                                </span>
+                                <span @click="$set(img, 'selected', true)" v-else class="badge bg-light rounded-pill text-dark float-end pr-2" style="line-height: 1.1;margin-right: 5px; margin-top: -25px;cursor: pointer">
+                                    <i class="mdi mdi-check"></i>
+                                </span>
+                            </h3>
+                            <p>{{ img.url }}</p>
+                        </div>
                     </template>
                     <template v-else>
-                        {{ limit(im.name, 20) }}...
+                        <img
+                            :src="lodash.find(img.subfiles, {'subselected' : true}).url"
+                            alt="..."
+                            class="d-block img-fluid"
+                        />
+                        <div class="carousel-caption d-none d-md-block card bg-dark" style="margin-bottom: -60px">
+                            <h3 class="text-white">{{ lodash.find(img.subfiles, {'subselected' : true}).name }}
+                                <span @click="$set(lodash.find(img.subfiles, {'subselected' : true}), 'selected', false)" v-if="lodash.find(img.subfiles, {'subselected' : true}).selected" class="badge bg-success rounded-pill float-end pr-2" style="line-height: 1.1;margin-right: 5px; margin-top: -25px;cursor: pointer"><i class="mdi mdi-check"></i></span>
+                                <span @click="$set(lodash.find(img.subfiles, {'subselected' : true}), 'selected', true)" v-else class="badge bg-light rounded-pill text-dark float-end pr-2" style="line-height: 1.1;margin-right: 5px; margin-top: -25px;cursor: pointer"><i class="mdi mdi-check"></i></span>
+                            </h3>
+                            <p>{{ img.url }}</p>
+                        </div>
                     </template>
-                    <i @click="deleteFile(im)"    class="mdi mdi-delete me-2 text-muted vertical-middle"></i>
-                </code>
-            </p>
+                </div>
+            <a
+                class="carousel-control-prev"
+                href="#"
+                role="button"
+                @click.prevent="list('back')"
+                data-bs-slide="prev"
+            >
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </a>
+            <a
+                class="carousel-control-next"
+                href="#"
+                role="button"
+                @click.prevent="list('next')"
+                data-bs-slide="next"
+            >
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </a>
+            </div>
+        </div>
+        </div>
+        <div class="col-3">
+            <div class="mt-4" style="max-height: 500px; overflow: hidden; overflow-y: scroll; text-align: center; background-color:#f3f7f9 !important; padding:20px;">
+                <div  v-for="(im, index) in activeImg ? lodash.find(images, {id: activeImg}).subfiles : []" style="cursor: pointer" :key="index" class="mb-2">
+                    <img @click="selectImg(im)" :src="im.url" alt="image" class="img-fluid rounded" width="200" :style="'height: 100px; width: auto; ' + (im.subselected ? '    border: 4px solid #1abc9c;' : '')">
+                    <p class="mb-0">
+                        {{ limit(im.name, 20) }}...
+                        <i @click="deleteFile(im)" class="mdi mdi-delete me-2 text-muted vertical-middle"></i>
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
-
-    <div
-    style="height: 600px"
-      id=""
-      class="carousel"
-    >
-      <div  class="carousel-inner" role="listbox">
-        <div v-for="img in selectedImage ? [selectedImage] : images" :class="'carousel-item ' + (activeImg == img.id ? ' active' : '')">
-            <template v-if="!lodash.find(img.subfiles, {'subselected' : true})">
-                <div
-                  v-if="img.extension == 'pdf'"
-                  style="width: 100%; height: 300px"
-                  class=" text-secondary rounded text-center"
-                >
-                  <i class="mdi mdi-file-pdf-outline font-28"></i>
-                </div>
-                <img
-                    v-else
-                    :src="img.url"
-                    alt="..."
-                    class="d-block img-fluid"
-                />
-                <div class="carousel-caption d-none d-md-block card bg-dark" style="margin-bottom: -60px">
-                    <h3 class="text-white">
-                        <template v-if="img.name.length < 40">
-                            {{ img.name }}
-                        </template>
-                        <template v-else>
-                            {{ limit(img.name, 40) }}...
-                        </template>
-                        <span @click="$set(img, 'selected', false)" v-if="img.selected" class="badge bg-success rounded-pill float-end pr-2" style="line-height: 1.1;margin-right: 5px; margin-top: -25px;cursor: pointer"><i class="mdi mdi-check"></i></span>
-                        <span @click="$set(img, 'selected', true)" v-else class="badge bg-light rounded-pill text-dark float-end pr-2" style="line-height: 1.1;margin-right: 5px; margin-top: -25px;cursor: pointer"><i class="mdi mdi-check"></i></span>
-                    </h3>
-                    <p>{{ img.url }}</p>
-                </div>
-            </template>
-            <template v-else>
-                <img
-                    :src="lodash.find(img.subfiles, {'subselected' : true}).url"
-                    alt="..."
-                    class="d-block img-fluid"
-                />
-                <div class="carousel-caption d-none d-md-block card bg-dark" style="margin-bottom: -60px">
-                    <h3 class="text-white">{{ lodash.find(img.subfiles, {'subselected' : true}).name }}
-                        <span @click="$set(lodash.find(img.subfiles, {'subselected' : true}), 'selected', false)" v-if="lodash.find(img.subfiles, {'subselected' : true}).selected" class="badge bg-success rounded-pill float-end pr-2" style="line-height: 1.1;margin-right: 5px; margin-top: -25px;cursor: pointer"><i class="mdi mdi-check"></i></span>
-                        <span @click="$set(lodash.find(img.subfiles, {'subselected' : true}), 'selected', true)" v-else class="badge bg-light rounded-pill text-dark float-end pr-2" style="line-height: 1.1;margin-right: 5px; margin-top: -25px;cursor: pointer"><i class="mdi mdi-check"></i></span>
-                    </h3>
-                    <p>{{ img.url }}</p>
-                </div>
-            </template>
-        </div>
-      <a
-        class="carousel-control-prev"
-        href="#"
-        role="button"
-        @click.prevent="list('back')"
-        data-bs-slide="prev"
-      >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </a>
-      <a
-        class="carousel-control-next"
-        href="#"
-        role="button"
-        @click.prevent="list('next')"
-        data-bs-slide="next"
-      >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </a>
-    </div>
-  </div>
   </div>
 </template>
 
@@ -165,7 +151,7 @@ export default {
                 this.images[_.findIndex(this.images, {id : this.activeImg }) + 1].id : this.images[0].id
             }
         },
-        limit (string = '', limit = 0) {  
+        limit (string = '', limit = 0) {
             return string.substring(0, limit)
         }
     },
