060b252aec4adca49e5fd1bcc404928b74169f98
Josip Peroš <josip@qedcode.net>
2022-04-09 19:17:34 +0200

    KC-BUGFIX fix for admin redirect

--- routes/general.php
+++ routes/general.php
@@ -1,5 +1,5 @@
 <?php
 use App\Http\Controllers\New_web\MainController;
 
-Route::get('/', [MainController::class, 'index']);
+Route::get('/', [MainController::class, 'index'])->name('homepage');
 Route::get('/{slug}', [MainController::class, 'page'])->name('new_general_page');
