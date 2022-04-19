f8dc0ea726184912dc05857d3062896c6663ec1c
Josip Peroš <josip@qedcode.net>
2022-04-19 18:11:07 +0200

    [KC-125] added cutom tokenizer

---
+++ app/Library/ImageTokenizer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Library;
+
+use TeamTNT\TNTSearch\Support\AbstractTokenizer;
+use TeamTNT\TNTSearch\Support\TokenizerInterface;
+
+class ImageTokenizer extends AbstractTokenizer implements TokenizerInterface
+{
+    protected static $pattern = '/[\s,\.-]+/';
+
+    public function tokenize($text, $stopwords)
+    {
+        return preg_split($this->getPattern(), strtolower($text), -1, PREG_SPLIT_NO_EMPTY);
+    }
+}
