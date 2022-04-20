4af8ae5248e184ce8d1bf1cd5c4653bb6bf4c3dc
Josip Peroš <josip@qedcode.net>
2022-04-20 18:37:39 +0200

    [KC-125] removing indexes from git

--- config/scout.php
+++ config/scout.php
@@ -141,7 +141,7 @@ return [
     */
 
     'tntsearch' => [
-        'storage'  => storage_path(), //place where the index files will be stored
+        'storage'  => storage_path() . "/indexes", //place where the index files will be stored
         'fuzziness' => env('TNTSEARCH_FUZZINESS', false),
         'fuzzy' => [
             'prefix_length' => 2,
