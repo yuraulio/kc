6a35e52d9e6e0624293eb4c7c611db6acf47b036
Josip Peroš <josip@qedcode.net>
2022-04-25 10:50:15 +0200

    [KC-118] refresh folders when new folder is added

--- resources/js/components/media/mediamixin.js
+++ resources/js/components/media/mediamixin.js
@@ -306,7 +306,8 @@ var mediaMixin = {
                     this.getFolders();
                     this.folderName = '';
                     this.loading = false;
-                    this.$modal.hide('create-folder-modal')
+                    this.$modal.hide('create-folder-modal');
+                    this.uncollapse(this.mediaFolders[0]);
                 })
                 .catch((error) => {
                     console.log(error)
