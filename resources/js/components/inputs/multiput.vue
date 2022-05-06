feb26ee72be97653f1d9c2f6ec37125e95ed09e7
Josip Peroš <josip@qedcode.net>
2022-05-06 13:49:22 +0200

    [KC-151] added suntitle to editor heading

--- resources/js/components/inputs/multiput.vue
+++ resources/js/components/inputs/multiput.vue
@@ -229,7 +229,17 @@ export default {
                         { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
                         { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
                         { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
-                        { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' }
+                        { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+                        {
+                            model: 'subtitle',
+                            view: {
+                                name: 'p',
+                                classes: 'editor-subtitle'
+                            },
+                            title: 'Subtitle',
+                            class: 'ck-heading_subtitle',
+                            converterPriority: 'high'
+                        }
                     ]
                 }
                 // The configuration of the editor.
