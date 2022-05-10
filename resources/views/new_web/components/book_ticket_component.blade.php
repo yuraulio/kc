9a741050681a7919d20f30323800651f717d62dd
Josip Peroš <josip@qedcode.net>
2022-05-10 16:52:23 +0200

    [KC-BUGFIX] disabled expired courses

--- resources/views/new_web/components/book_ticket_component.blade.php
+++ resources/views/new_web/components/book_ticket_component.blade.php
@@ -4,6 +4,7 @@
     $event = $dynamic_page_data["event"] ?? null;
     $sections = $dynamic_page_data["sections"] ?? null;
     $showAlumni = $dynamic_page_data["showAlumni"] == null;
+    $estatus = $event->status ?? null;
 @endphp
 
 <?php
@@ -16,108 +17,108 @@
 ?>
 
 @if ($tickets)
-    
-<section id="seats" class="section-tickets">
-   <div class="container">
-        <h2 class="section-title">{{$title}}</h2>
-        <div class="row row-flex row-flex-15">
-            <?php 
-               $early = false;
-               $index = 0;
-            ?>
-            @foreach($tickets as $key => $ticket)
-                <?php 
-                    $options = json_decode($ticket['pivot']['options'],true); 
-                    if($ticket['type'] == 'Early Bird' && $ticket['pivot']['quantity'] > 0){
-                        $early = true;
-                    }else if($ticket['type'] == 'Early Bird'){
-                        continue;
-                    }
-                    if($ticket['type'] == 'Special' && $early){
-                        continue;
-                    } 
-                    if($ticket['type'] == 'Sponsored'){
-                        continue;
-                    } 
-                    if($ticket['type'] == 'Alumni' && (!Auth::user() || (Auth::user() && !Auth::user()->kc_id))){  
-                        $showAlumni = false;
-                        continue;
-                    }
-                    $index = $index + 1;
-                ?>
-                <div class="col-md-6 col-sm-12 d-flex book-ticket-boxes mb-4 {{ $index % 2 != 0 ? 'justify-content-end-2' : 'justify-content-start-2' }}" >
-                    <div class="ticket-box-wrapper" style="width: 100%;">
-                        <div class="ticket-box">
-                            <h3 class="@if($ticket['type'] != 'Alumni') special-ticket @endif">{{ $ticket['type'] }} <span> €{{$ticket['pivot']['price']}} </span></h3>
-                            <div class="ticket-box-content">
-                                <ul class="seat-features">
-                                    @foreach((array) json_decode($ticket['pivot']['features']) as $feature)
-                                    <li>{{ $feature }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                            <div class="ticket-box-price">
-                                <span class="ticket-price hidden-xs">€{{$ticket['pivot']['price']}}</span>
-                                <span class="ticket-infos"> {{ $ticket['subtitle'] }}</span>
-                                <span class="ticket-infos hidden-xs">@if($ticket['type'] != 'Alumni' && ($event->view_tpl!='elearning_event')) {{ $ticket['pivot']['quantity'] }} seats remaining @else &nbsp; @endif</span> 
-                            </div>
-                            <div class="ticket-box-actions">
-                                @if($ticket['type'] == 'Regular' && $early)
-                                    @if($ticket['pivot']['quantity'] <= 0)
-                                        <div class="btn btn--lg btn--secondary btn--sold-out">SOLD-OUT</div>
-                                    @else
-                                        <div class="btn btn--lg btn--secondary btn--completed" >AVAILABLE SOON</div>
-                                    @endif
-                                @else
-                                    @if(isset($options['dropdown']) && $options['dropdown'])
-                                        @if($ticket['pivot']['quantity'] <= 0)
-                                            <div class="btn btn--lg btn--secondary btn--sold-out">SOLD-OUT</div>
+    @if ($estatus == 0 || $estatus == 2 || $estatus == 5)
+        <section id="seats" class="section-tickets">
+        <div class="container">
+                <h2 class="section-title">{{$title}}</h2>
+                <div class="row row-flex row-flex-15">
+                    <?php 
+                    $early = false;
+                    $index = 0;
+                    ?>
+                    @foreach($tickets as $key => $ticket)
+                        <?php 
+                            $options = json_decode($ticket['pivot']['options'],true); 
+                            if($ticket['type'] == 'Early Bird' && $ticket['pivot']['quantity'] > 0){
+                                $early = true;
+                            }else if($ticket['type'] == 'Early Bird'){
+                                continue;
+                            }
+                            if($ticket['type'] == 'Special' && $early){
+                                continue;
+                            } 
+                            if($ticket['type'] == 'Sponsored'){
+                                continue;
+                            } 
+                            if($ticket['type'] == 'Alumni' && (!Auth::user() || (Auth::user() && !Auth::user()->kc_id))){  
+                                $showAlumni = false;
+                                continue;
+                            }
+                            $index = $index + 1;
+                        ?>
+                        <div class="col-md-6 col-sm-12 d-flex book-ticket-boxes mb-4 {{ $index % 2 != 0 ? 'justify-content-end-2' : 'justify-content-start-2' }}" >
+                            <div class="ticket-box-wrapper" style="width: 100%;">
+                                <div class="ticket-box">
+                                    <h3 class="@if($ticket['type'] != 'Alumni') special-ticket @endif">{{ $ticket['type'] }} <span> €{{$ticket['pivot']['price']}} </span></h3>
+                                    <div class="ticket-box-content">
+                                        <ul class="seat-features">
+                                            @foreach((array) json_decode($ticket['pivot']['features']) as $feature)
+                                            <li>{{ $feature }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                    <div class="ticket-box-price">
+                                        <span class="ticket-price hidden-xs">€{{$ticket['pivot']['price']}}</span>
+                                        <span class="ticket-infos"> {{ $ticket['subtitle'] }}</span>
+                                        <span class="ticket-infos hidden-xs">@if($ticket['type'] != 'Alumni' && ($event->view_tpl!='elearning_event')) {{ $ticket['pivot']['quantity'] }} seats remaining @else &nbsp; @endif</span> 
+                                    </div>
+                                    <div class="ticket-box-actions">
+                                        @if($ticket['type'] == 'Regular' && $early)
+                                            @if($ticket['pivot']['quantity'] <= 0)
+                                                <div class="btn btn--lg btn--secondary btn--sold-out">SOLD-OUT</div>
+                                            @else
+                                                <div class="btn btn--lg btn--secondary btn--completed" >AVAILABLE SOON</div>
+                                            @endif
                                         @else
+                                            @if(isset($options['dropdown']) && $options['dropdown'])
+                                                @if($ticket['pivot']['quantity'] <= 0)
+                                                    <div class="btn btn--lg btn--secondary btn--sold-out">SOLD-OUT</div>
+                                                @else
 
-                                        <div class="ticket-actions-wrapper">
-                                            <a href="#" class="btn btn--lg btn--primary btn-ticket--dropdown">ENROLL NOW</a>
-                                            <ul class="tickets-dropdown">
-                                                @if($ticket['type'] != 'Alumni') 
-                                                <li><a href="{{ route('cart.add-item', [ $event->id, $ticket['id'], 1 ]) }}" class="btn btn-add btn--lg btn--primary">UNEMPLOYED</a></li>
-                                                <li><a href="{{ route('cart.add-item', [ $event->id, $ticket['id'], 2 ]) }}" class="btn btn-add btn--lg btn--primary">STUDENT</a></li>
-                                                <li><a href="{{ route('cart.add-item', [ $event->id, $ticket['id'], 5 ]) }}" class="btn btn-add btn--lg btn--primary">GROUP</a></li>
-                                                @endif
-                                            </ul>
-                                        </div>
-                                    @endif
-                                    @else
-                                        @if($ticket['pivot']['quantity'] <= 0)
-                                            <div class="btn btn--lg btn--secondary btn--sold-out">SOLD-OUT</div>
-                                        @else
-                                            @if($ticket['pivot']['price'] == 0)
                                                 <div class="ticket-actions-wrapper">
                                                     <a href="#" class="btn btn--lg btn--primary btn-ticket--dropdown">ENROLL NOW</a>
                                                     <ul class="tickets-dropdown">
-                                                        <li><a class="btn btn-add btn--lg btn--primary" title="ENROLL NOW" href="{{ route('cart.add-item', [ $event->id, $ticket['id'], 3 ]) }}">KNOWCRUNCH ALUMNI</a></li>
-                                                        <li><a class="btn btn-add btn--lg btn--primary" title="ENROLL NOW" href="{{ route('cart.add-item', [ $event->id, $ticket['id'], 0 ]) }}">OTHER</a></li>
+                                                        @if($ticket['type'] != 'Alumni') 
+                                                        <li><a href="{{ route('cart.add-item', [ $event->id, $ticket['id'], 1 ]) }}" class="btn btn-add btn--lg btn--primary">UNEMPLOYED</a></li>
+                                                        <li><a href="{{ route('cart.add-item', [ $event->id, $ticket['id'], 2 ]) }}" class="btn btn-add btn--lg btn--primary">STUDENT</a></li>
+                                                        <li><a href="{{ route('cart.add-item', [ $event->id, $ticket['id'], 5 ]) }}" class="btn btn-add btn--lg btn--primary">GROUP</a></li>
+                                                        @endif
                                                     </ul>
                                                 </div>
+                                            @endif
                                             @else
-                                                @if($ticket['type'] == 'Alumni')
-                                                    <div class="ticket-actions-wrapper">
-                                                        <a href="{{ route('cart.add-item', [ $event->id, $ticket['id'], 3 ]) }}" class="btn btn-add btn--lg btn--primary ">ENROLL NOW</a>
-                                                    </div>                                      
+                                                @if($ticket['pivot']['quantity'] <= 0)
+                                                    <div class="btn btn--lg btn--secondary btn--sold-out">SOLD-OUT</div>
                                                 @else
-                                                    <div class="ticket-actions-wrapper">
-                                                        <a class="btn btn-add btn--lg btn--primary"  title="ENROLL NOW" href="{{ route('cart.add-item', [ $event->id, $ticket['id'], 0 ]) }}">ENROLL NOW</a>
-                                                    </div>
+                                                    @if($ticket['pivot']['price'] == 0)
+                                                        <div class="ticket-actions-wrapper">
+                                                            <a href="#" class="btn btn--lg btn--primary btn-ticket--dropdown">ENROLL NOW</a>
+                                                            <ul class="tickets-dropdown">
+                                                                <li><a class="btn btn-add btn--lg btn--primary" title="ENROLL NOW" href="{{ route('cart.add-item', [ $event->id, $ticket['id'], 3 ]) }}">KNOWCRUNCH ALUMNI</a></li>
+                                                                <li><a class="btn btn-add btn--lg btn--primary" title="ENROLL NOW" href="{{ route('cart.add-item', [ $event->id, $ticket['id'], 0 ]) }}">OTHER</a></li>
+                                                            </ul>
+                                                        </div>
+                                                    @else
+                                                        @if($ticket['type'] == 'Alumni')
+                                                            <div class="ticket-actions-wrapper">
+                                                                <a href="{{ route('cart.add-item', [ $event->id, $ticket['id'], 3 ]) }}" class="btn btn-add btn--lg btn--primary ">ENROLL NOW</a>
+                                                            </div>                                      
+                                                        @else
+                                                            <div class="ticket-actions-wrapper">
+                                                                <a class="btn btn-add btn--lg btn--primary"  title="ENROLL NOW" href="{{ route('cart.add-item', [ $event->id, $ticket['id'], 0 ]) }}">ENROLL NOW</a>
+                                                            </div>
+                                                        @endif
+                                                    @endif
                                                 @endif
                                             @endif
                                         @endif
-                                    @endif
-                                @endif
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                </div>        
-            @endforeach   
-        </div>
-    </div>
-</section>
-
+                        </div>        
+                    @endforeach   
+                </div>
+            </div>
+        </section>
+    @endif
 @endif
--- resources/views/new_web/components/tabs_component.blade.php
+++ resources/views/new_web/components/tabs_component.blade.php
@@ -45,11 +45,17 @@
 
                             <a href="#" class="mobile-tabs-menu">Overview</a>
                             <ul class="clearfix tab-controls-list">
-                                @foreach ($tabs["tabs"]["tabs"] as $index=>$tab)
-                                    @if (checkTabContent($tab, $dynamic_page_data, $tabs))
-                                        <li><a href="#{{Illuminate\Support\Str::slug($tab)}}" class="{{$index == 0 ? "active" : "" }}">{{$tab}}</a></li>
-                                    @endif
-                                @endforeach
+                                @if ($estatus == 0 || $estatus == 2 || $estatus == 5)
+                                    @foreach ($tabs["tabs"]["tabs"] as $index=>$tab)
+                                        @if (checkTabContent($tab, $dynamic_page_data, $tabs))
+                                            <li><a href="#{{Illuminate\Support\Str::slug($tab)}}" class="{{$index == 0 ? "active" : "" }}">{{$tab}}</a></li>
+                                        @endif
+                                    @endforeach
+                                @else
+                                    <li><a href="#{{Illuminate\Support\Str::slug("Overview")}}" class="active">Overview</a></li>
+                                    <li><a href="#{{Illuminate\Support\Str::slug("Topics")}}" class="">Topics</a></li>
+                                    <li><a href="#{{Illuminate\Support\Str::slug("Instructors")}}" class="">Instructors</a></li>
+                                @endif
                             </ul>
                             
                             @if ($event)
@@ -60,7 +66,11 @@
                                         <a href="{{ route('enrollForFree',  $event->id) }}" class="btn btn--lg btn--primary go-to-href">ENROLL FOR FREE</a>
                                     @endif
                                 @elseif($estatus == 0 && !$is_event_paid)
-                                    <a href="#seats" class="btn btn--lg btn--primary go-to-href">ENROLL NOW</a>
+                                    @if($is_event_paid==0 && $event['view_tpl'] == 'event_free_coupon')
+                                        <a href="javascript:void(0)" id="open-code-popup" class="btn btn--lg btn--primary">ENROLL NOW</a>
+                                    @else
+                                        <a href="#seats" class="btn btn--lg btn--primary go-to-href">ENROLL NOW</a>
+                                    @endif
                                 @elseif($estatus == 5 && !$is_joined_waiting_list && !$is_event_paid)
                                     <a href="{{ route('cart.add-item', [ $event->id,'waiting', 8 ]) }}" class="btn btn--lg btn--primary go-to-href elearning-free">JOIN WAITING LIST</a>
                                 @elseif($estatus != 3 && $estatus != 5 && $estatus != 1 && !$is_event_paid)
@@ -71,9 +81,15 @@
                 </div>
             </div>
 
+            @if ($estatus == 1 || $estatus == 3)
+                @php
+                    array_shift($tabs["tabs"]["tabs"]);
+                @endphp
+            @endif
+
             <div class="tabs-content">
                 @foreach ($tabs["tabs"]["tabs"] as $index => $tab)
-                    <div id="{{Illuminate\Support\Str::slug($tab)}}" class="p-0 tab-content-wrapper {{ $index == 0 ? 'active-tab' : '' }}">
+                    <div id="{{Illuminate\Support\Str::slug($tab)}}" class="p-0 tab-content-wrapper {{ Illuminate\Support\Str::slug($tab) == "overview" ? 'active-tab' : '' }}">
                         @foreach ($tabs["tabs"]["value"] as $data)
                             @if ($data->tabs_tab == $tab)
                                 @include("new_web.layouts.rows")
@@ -83,6 +99,80 @@
                 @endforeach
             </div>
 
+            @if ($estatus == 1 || $estatus == 3)
+                <div class="container">
+                <div  class="course-overview clearfix mt-5 mb-5">
+                    <div class="course-tab-text" itemprop="abstract">
+
+                        <span class="completed">The event is completed</span>
+                        <p style="display:none">The best digital &amp; social media diploma with a long track record trusted by top executives, agencies, brands and corporations is completed.</p>
+                        <p >Please check all our @if(isset($location->name)) <a href="{{ $location->slug }}">upcoming events in {{ $location->name }}</a> @else upcoming events in this city @endif.</p>
+                    </div>
+                </div>
+                </div>
+            @endif
+
         </div>
     </div>
 </div>
+
+<div class="code-popup-wrapper">
+   <div class="code-popup">
+      <a href="javascript:void(0)" class="close-btn"><img width="26" src="{{cdn('theme/assets/images/icons/icon-close.svg')}}" class="replace-with-svg" alt="Close"></a>
+      <div class="heading">
+         <span>G-app Registration</span>
+         <p>This event is only for students who have a special coupon by the Germanos G-app.</p>
+      </div>
+      <div class="alert-outer" hidden>
+      <div class="container">
+         <div class="alert-wrapper error-alert">
+            <div class="alert-inner">
+               <p id="code-error"></p>
+            </div>
+         </div>
+      </div>
+      <!-- /.alert-outer -->
+	</div>
+         <div class="input-wrapper input-wrapper--text">
+               <input type="text" onblur="this.placeholder = 'Please type your code here'" onfocus="this.placeholder = ''" placeholder="Please type your code here" id="event-code" name="event-code">
+         </div>
+         <input type="hidden" value="{{$event->id}}" name="event">
+         <input type="submit" onclick="checkCode()"  value="Confirm">
+   </div><!-- ./code-popup -->
+</div><!-- ./code-popup-wrapper -->
+
+@push('components-scripts')
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#open-code-popup').click(function(e) {
+                    $('.code-popup-wrapper').addClass('active');
+                });
+
+            $(".close-btn").click(function(){
+                $('.code-popup-wrapper').removeClass('active');
+                $('.alert-outer').hide()
+            })
+
+            function checkCode(){
+                $('.alert-outer').hide()
+                var eventCode = $('#event-code').val();
+                var event = $('input[name=event]').val();
+                $.ajax({ url: '/checkCode', type: "post",
+                    data: {eventCode:eventCode, event:event},
+                    success: function(data) {
+                        if(!data['success']){
+                        var p = document.getElementById('code-error').textContent = data['message'];
+                        var img = document.createElement('img');
+                        img.setAttribute('src',"/theme/assets/images/icons/alert-icons/icon-error-alert.svg" )
+                        img.setAttribute('alt',"Info Alert" )
+                        $('#code-error').append(img);
+                        $('.alert-outer').show()
+                        }else{
+                            window.location.replace(data['redirect']);
+                        }    
+                    },
+                });
+            }
+        });
+    </script>
+@endpush
