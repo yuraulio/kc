8985eb1f94a985474707cfdd8afae66acaa152bc
Josip Peroš <josip@qedcode.net>
2022-05-24 09:18:42 +0200

    [KC-152] added webpack configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -91,3 +91,11 @@ mix.js('resources/js/app.js', 'public/js')
 // MediaManager
 mix.sass('resources/assets/vendor/MediaManager/sass/manager.scss', 'public/assets/vendor/MediaManager/style.css')
     .copyDirectory('resources/assets/vendor/MediaManager/dist', 'public/assets/vendor/MediaManager')
+
+mix.webpackConfig({
+    watchOptions: {
+        ignored: /node_modules/,
+        aggregateTimeout: 200,
+        poll: 1000,
+    }
+});
