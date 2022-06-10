56b7acad9eabd2b7ba036ccee79e5f8bbef90777
unknown <AntonisMoul>
2022-06-10 17:54:06 +0300

    delete upcoming invoices with unused time

--- app/Console/Commands/UserBalanceStripe.php
+++ app/Console/Commands/UserBalanceStripe.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use App\Model\User;
 use App\Model\PaymentMethod;
 use Stripe\Stripe;
+use \Stripe\StripeClient;
 
 class UserBalanceStripe extends Command
 {
@@ -41,91 +42,29 @@ class UserBalanceStripe extends Command
     public function handle()
     {
 
-        $users = User::whereHas('subscriptions')->get();//where('id',4201)->get();//User::all();
-			//$id = 'ii_1KJfI1IppxFyGi0YGCFL4Ugj';
-        foreach($users as $user){
-		
-            if(!$user->stripe_id){
-                continue;
-            }
-
-            $paymentMethod = PaymentMethod::find(2);
-            //dd($paymentMethod);
-            Stripe::setApiKey($paymentMethod->processor_options['secret_key']);
-            session()->put('payment_method',2);
-
+      $paymentMethod = PaymentMethod::find(2);
          
-          try{
-            if(!$user->asStripeCustomer()){
-           		continue; 
-          	}
-            //dd($user);
-            //$user->applyBalance(100);
-            //dd($user);
-            
-            if($user->upcomingInvoice() && $user->upcomingInvoice()->total < 0){
-              
-              //dd($user->upcomingInvoice()->invoiceLineItems());
-              
-              /*foreach($user->upcomingInvoice()->invoiceLineItems() as $invoice){
-                $invoice->void();
-              }*/
-              //dd($user->upcomingInvoice());
-              //$user->upcomingInvoice()->markUncollectible();
-              //$user->upcomingInvoice()->void();
-              //dd($user->upcomingInvoice());
-              //$user->upcomingInvoice()->finalize();
-              
-              //dd('fsdfa');
-              
-              //$user->upcomingInvoice()->void();
-              //$user->applyBalance($user->upcomingInvoice()->total);
-             
-             
-            }
-            
-            if(!empty($user->invoicesIncludingPending())){
-              $amount = 0;
-              foreach($user->invoicesIncludingPending() as $invoice){
-               
-                if($invoice->total < 0 ){
-                  //dd($invoice);
-                  //$inv = $invoice->markUncollectible();
-                  //dd($inv);
-                  $invoice->void(['consume_applied_balance'=>false]);
-                  //$invoice->save();
-                  //print_r('is void = '. $invoice->isVoid());
-                  
-                  //$invoice->void();
-                  //dd($invoice);
-                 //$amount += $invoice->total;
-                }
-                             
-              	
-              }
-              
-              /*if($amount != 0){
-               $user->applyBalance($amount); 
-              }*/
-              
-            
-              
-            }
-            
-            
-          }catch(\Stripe\Exception\InvalidRequestException $e){
-           	
-            continue;
-            
-          }
-          catch(\InvalidArgumentException  $e){
-           	
-            continue;
-            
-          }
    
+       $stripe = new StripeClient(array_merge([
+            'api_key' => $paymentMethod->processor_options['secret_key'],
+            'stripe_version' => '2020-08-27',
+        ], []));
+      
+      
+      //dd($stripe->invoiceItems->all(['pending' => true]));
+      
+      	foreach($stripe->invoiceItems->all(['pending' => true]) as $invoice){
+          
+          if($invoice->amount < 0){
+            //dd($invoice);
+            $stripe->invoiceItems->delete(
+			        $invoice->id,
+  			      []
+			      );
+          }
+          	
         }
-
+      
         return 0;
     }
 }
