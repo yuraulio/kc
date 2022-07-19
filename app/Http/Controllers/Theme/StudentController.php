aef4fcb5fe5dd489e37f57c7fcf68f87c5af9256
unknown <AntonisMoul>
2022-07-19 16:40:26 +0300

    enable subcription

--- app/Http/Controllers/Theme/StudentController.php
+++ app/Http/Controllers/Theme/StudentController.php
@@ -663,7 +663,7 @@ class StudentController extends Controller
 
                 if($expiration_event){
         
-                    $after20Days  = strtotime("+20 day");
+                    $after20Days  = strtotime("+22 day");
                 
                 }
                 
