a6c3b49eb73c0a110e16010e14543e50ac63ea3e
unknown <AntonisMoul>
2022-07-19 16:57:11 +0300

    enable subcription

--- app/Http/Controllers/Theme/SubscriptionController.php
+++ app/Http/Controllers/Theme/SubscriptionController.php
@@ -378,6 +378,25 @@ class SubscriptionController extends Controller
                 //$subEnds = $plan->trial_days && $plan->trial_days > 0 ? $plan->trial_days : $plan->getDays();
                 $subEnds = $plan->getDays();
                 $subEnds=date('d-m-Y', strtotime("+$subEnds days"));
+                
+                if($exp = $user->events()->wherePivot('event_id',$event->id)->first()){
+				
+                    $exp = $exp->pivot->expiration;
+                    $exp = strtotime($exp);
+                    $today = strtotime(date('Y-m-d'));
+    
+                    if($exp && $exp > $today){
+    
+                        $exp = date_create(date('Y-m-d',$exp));
+                        $today = date_create(date('Y-m-d',$today));
+    
+                        $days = date_diff($exp, $today);
+    
+                        $subEnds = date('Y-m-d', strtotime($subEnds. ' + ' . $days->d .' days'));
+    
+                    }
+    
+                }
 
                 $data['firstName'] = $user->firstname;
 
