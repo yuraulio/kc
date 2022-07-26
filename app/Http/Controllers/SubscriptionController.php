a30ff0ce61b431b8bb1f9110694b932d33564ecc
pavlostiftikidis <pavlostif@gmail.com>
2022-07-26 16:42:12 +0300

    push

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Laravel\Cashier\Subscription;
 use App\Model\Transaction;
+use App\Model\Event;
 use Excel;
 use App\Exports\SubscriptionExport;
 
@@ -17,8 +18,12 @@ class SubscriptionController extends Controller
     {
         $subscriptions = [];
 
-        $data['subscriptions'] = Transaction::with('user', 'subscription.event', 'subscription.event.category')->get();
-        //$plans = Plan::all()->groupby('stripe_plan');
+        $events = Event::has('transactions')->with('category', 'delivery')->get()->groupBy('id');
+        $data['subscriptions'] = Transaction::with('user', 'subscription.event', 'subscription.event.category')->get()->sortBy('created_at');
+        // $plans = Plan::all()->groupby('stripe_plan');
+        $plans = Plan::with('categories')->get()->groupBy('id');
+        //dd($plans);
+        $users = [];
 
         foreach($data['subscriptions'] as $key => $sub){
 
@@ -28,22 +33,13 @@ class SubscriptionController extends Controller
                     continue;
                 }
 
-                /*
-                $event_category = $sub['subscription'][0]['event'][0]['category'][0]['id'];
-                // 183
-                //dd($event_category);
-                $plan = $sub['subscription'][0]['event'][0]['plans']->last();
-                $plan_categories = $plan->categories;
-                //dd($plan_categories);
-                // 46, 183, 277
-
-                foreach($plan_categories as $plan_cat){
-                    if($plan_cat['id'] == $event_category){
-                        $plan_category = $plan_cat;
-                    }
+                if(isset($users[$sub['user'][0]['id']])){
+                    $users[$sub['user'][0]['id']][] = $sub['subscription'][0]['event'][0]['id'];
+                }else{
+                    $users[$sub['user'][0]['id']] = [];
+                    $users[$sub['user'][0]['id']][] = $sub['subscription'][0]['event'][0]['id'];
                 }
 
-                */
                 $status = $sub['subscription'][0]['stripe_status'];
 
                 if($sub['trial'] && $status == 'trialing'){
@@ -58,10 +54,47 @@ class SubscriptionController extends Controller
 
                 $name = $sub['user'][0]['firstname'] . ' ' . $sub['user'][0]['lastname'];
                 $amount = '€'.number_format(intval($sub['total_amount']), 2, '.', '');
+                //$delivery = $sub['subscription'][0]['event'][0]['delivery'][0]['name'];
 
-                $subscriptions[$sub['subscription'][0]['stripe_id']]=['user' => $name, 'plan_name' => $sub['subscription'][0]['name'],
+                //dd($sub);
+
+                $subscriptions[$sub['subscription'][0]['stripe_id']]=['user_id' => $sub['user'][0]['id'], 'user' => $name, 'plan_id' => $sub['subscription'][0]['event'][0]['plans'][0]['id'], 'plan_name' => $sub['subscription'][0]['name'],
                     'event_title' => $sub['subscription'][0]['event'][0]['title'], 'status' => $status,'ends_at'=>$sub['ends_at'],
-                    'amount' => $amount,'created_at'=>date('Y-m-d',strtotime($sub['created_at'])),'id'=>$sub['id']];
+                    'amount' => $amount,'created_at'=>date('Y-m-d',strtotime($sub['created_at'])),'id'=>$sub['id'], 'event_id' => $sub['subscription'][0]['event'][0]['id']];
+
+            }
+
+
+        }
+
+
+        foreach($users as $key => $user){
+            if(count($user) != 0){
+                $users[$key] = $user[count($user) - 1];
+            }
+
+        }
+
+        //dd($subscriptions);
+
+        foreach($subscriptions as $key => $sub){
+
+            $userEvent = $users[$sub['user_id']];
+
+            if(!empty($userEvent)){
+                $event = $events[$userEvent][0];
+                $category = $event['category'][0];
+
+                $hasFind = false;
+                foreach($plans[$sub['plan_id']][0]['categories'] as $plan_cat){
+                    if($plan_cat['id'] == $category['id']){
+                        $hasFind = true;
+                    }
+                }
+                if($hasFind){
+                    $subscriptions[$key]['delivery']['id'] = $event['delivery'][0]['id'];
+                    $subscriptions[$key]['delivery']['name'] = $event['delivery'][0]['name'];
+                }
 
             }
 
