c50c4b607d40f5ac536cbd5cf57448dc7cb13212
unknown <AntonisMoul>
2022-07-26 10:32:44 +0300

    admin new settings

--- app/Library/CMS.php
+++ app/Library/CMS.php
@@ -13,7 +13,7 @@ class CMS
     {
         $data = $event->topicsLessonsInstructors();
         $data['event'] = $event;
-        $data['benefits'] = $event->benefits->toArray();
+        $data['benefits'] = [];//$event->benefits->toArray();
         //$data['summary'] = $event->summary1()->get()->toArray();
         $data['sections'] = $event->sections->groupBy('section');
         $data['section_fullvideo'] = $event->sectionVideos->first();
