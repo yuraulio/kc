8595cc2ce847935fffdb6a3f3d916f71b80403ba
AntonisMoul <antoniskfl@gmail.com>
2022-10-19 15:29:16 +0300

    transaction  for free events

--- app/Console/Commands/ImportFaqs.php
+++ app/Console/Commands/ImportFaqs.php
@@ -119,7 +119,7 @@ class ImportFaqs extends Command
             $file1 = $file->getSheet($key);
             $file1 = $file1->toArray();
             
-            /*foreach($file1 as $key1 =>  $line){
+            foreach($file1 as $key1 =>  $line){
 
                 if($key1 == 0 ){
                     continue;
@@ -142,10 +142,10 @@ class ImportFaqs extends Command
                 $faq->event()->attach($events,['priority' => $key1]);
 
 
-            }*/
+            }
 
             
-            foreach($file1 as $key1 =>  $line){
+            /*foreach($file1 as $key1 =>  $line){
                 if($key == 1){
                     echo $key1;
                 }
@@ -183,7 +183,7 @@ class ImportFaqs extends Command
                 $faq->event()->sync($events,['priority' => $key1]);
 
 
-            }
+            }*/
 
 
 
