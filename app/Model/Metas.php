5f52fe3bf945875c602a22920b2e3ae390fd8b17
pavlostiftikidis <pavlostif@gmail.com>
2023-05-12 15:23:14 +0300

    meta version image

--- app/Model/Metas.php
+++ app/Model/Metas.php
@@ -19,27 +19,27 @@ class Metas extends Model
         //to do
         //dd($this);
 
-        $pageLink = !$isHomePage ? url('/') . '/'  . $this->metable->slugable->slug :  url('/'); 
+        $pageLink = !$isHomePage ? url('/') . '/'  . $this->metable->slugable->slug :  url('/');
         $title =  $this->meta_title ?  $this->meta_title : $this->metable->name;
 
-        $metas = 
+        $metas =
             '<title>'. $title .'</title>
             <meta name="author" content="Knowcrunch">
             <meta name="description" content="' . $this->meta_description .'">
             <meta name="keywords" content="' . $this->meta_keywords .'">
-            <meta property="fb:app_id" content="961275423898153">' . 
+            <meta property="fb:app_id" content="961275423898153">' .
             '<meta property="og:title" content="' . $this->meta_title .'">'.
             '<meta property="og:type" content="website">' .
             '<meta property="og:url" content="' . $pageLink  .'">' .
-            '<meta property="og:image" content="' . url('/')  . get_image($this->metable->mediable,'social-media-sharing') .'">'.
+            '<meta property="og:image" content="' . url('/')  . get_image($this->metable->mediable,'header-image') .'">'.
             '<meta property="og:site_name" content="' . $this->meta_title .'">'.
             '<meta property="og:description" content="' . $this->meta_description .'">'.
             '<meta name="twitter:card" content="summary_large_image">'.
             '<meta name="twitter:title" content="' . $this->meta_title .'">'.
             '<meta name="twitter:description" content="' . $this->meta_description .'">'.
-            '<meta name="twitter:image" content="' . url('/')  . get_image($this->metable->mediable,'social-media-sharing') .'">';
+            '<meta name="twitter:image" content="' . url('/')  . get_image($this->metable->mediable,'header-image') .'">';
 
-        
+
         return $metas;
 
     }
