f47987485a51256146326246c6bf5fa72073378d
korchevskiy <sergey.korchevskiy@gmail.com>
2024-02-06 11:07:25 +0200

    feat(Reports): change the name for BigElearning report [WEB-127] (#64)

--- app/Http/Controllers/Admin_api/ExportController.php
+++ app/Http/Controllers/Admin_api/ExportController.php
@@ -5,14 +5,12 @@ namespace App\Http\Controllers\Admin_api;
 use App\Exports\BigElearningNoSubscriptionExpired;
 use App\Http\Controllers\Controller;
 use Excel;
-use Exception;
-use Illuminate\Http\JsonResponse;
 
 class ExportController extends Controller
 {
     public function subscriptionEnd()
     {
-        $filename = 'users_masterclass_big_elearning_no_subsctription_expired_access_completed.xlsx';
+        $filename = 'get_1_year_access.xlsx';
 
         //Excel::store(new BigElearningNoSubscriptionExpired(), $filename, 'export');
         return Excel::download(new BigElearningNoSubscriptionExpired(), $filename);
---
+++ resources/lang/en/reports.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+    'export_title' => 'All the people that were in a class course of Digital & Social Media Marketing, all the people that bought the E-Learning Masterclass in Digital & Social Media Marketing (old & new) and none of them have active subscription or access to the big E-Learning course right now.',
+
+];
