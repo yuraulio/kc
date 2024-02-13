8902b647f9661814ce098b7af395f84e829697a8
Lucas Muley <50708774+lucasmuleyv@users.noreply.github.com>
2024-02-13 16:52:27 +0100

    feat: add migration (#91)

---
+++ database/migrations/2024_02_13_174951_add_fields_to_event_statistics_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('event_statistics', function (Blueprint $table) {
+            $table->float('total_seen')->default(0)->change();
+            $table->float('total_duration')->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('event_statistics', function (Blueprint $table) {
+            $table->double('total_seen', 8, 2)->default(0)->change();
+            $table->double('total_duration', 8, 2)->default(0)->change();
+        });
+    }
+};
