6821925602f8c4e1ea8fbd0c27ead9d6f3027b79
Lucas Muley <50708774+lucasmuleyv@users.noreply.github.com>
2024-02-15 18:48:26 +0100

    feat: correct widget subscribers -> correct the list of the subscriber to be unique[153] (#100)

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -18,7 +18,7 @@ class SubscriptionController extends Controller
         $subscriptions = [];
 
         $events = Event::has('transactions')->with('category', 'delivery')->get()->groupBy('id');
-        $data['subscriptions'] = Transaction::with('user', 'user.events_for_user_list', 'subscription.event', 'subscription.event.category')->get()->sortBy('created_at');
+        $transactions = Transaction::with('user', 'user.events_for_user_list', 'subscription.event', 'subscription.event.category')->get()->sortBy('created_at');
         // $plans = Plan::all()->groupby('stripe_plan');
         //$plans = Plan::with('categories')->get()->groupBy('id');
         $plans = Plan::wherePublished(1)->with('categories')->get();
@@ -33,60 +33,62 @@ class SubscriptionController extends Controller
         $data['total_users'] = 0;
         $total_users = [];
 
-        foreach ($data['subscriptions'] as $key => $sub) {
+        foreach ($transactions as $key => $tra) {
             $events = [];
-            if (count($sub['subscription']) != 0) {
-                if (!isset($sub['subscription'][0]['event'][0]['title'])) {
+            // dump($tra['subscription']);
+            // dump(count($tra['subscription']));
+            if (count($tra['subscription']) != 0) {
+                if (!isset($tra['subscription'][0]['event'][0]['title'])) {
                     $subEvId = -1;
                     $titleE = '-';
                     $plId = -1;
                 } else {
-                    $subEvId = $sub['subscription'][0]['event'][0]['id'];
-                    $titleE = $sub['subscription'][0]['event'][0]['title'];
-                    $plId = $sub['subscription'][0]['event'][0]['plans'][0]['id'];
+                    $subEvId = $tra['subscription'][0]['event'][0]['id'];
+                    $titleE = $tra['subscription'][0]['event'][0]['title'];
+                    $plId = $tra['subscription'][0]['event'][0]['plans'][0]['id'];
                 }
 
-                if (isset($users[$sub['user'][0]['id']])) {
-                    $users[$sub['user'][0]['id']][] = $subEvId;
+                if (isset($users[$tra['user'][0]['id']])) {
+                    $users[$tra['user'][0]['id']][] = $subEvId;
                 } else {
-                    $users[$sub['user'][0]['id']] = [];
-                    $users[$sub['user'][0]['id']][] = $subEvId;
+                    $users[$tra['user'][0]['id']] = [];
+                    $users[$tra['user'][0]['id']][] = $subEvId;
                 }
 
-                $status = $sub['subscription'][0]['stripe_status'];
+                $status = $tra['subscription'][0]['stripe_status'];
 
-                if ($sub['trial'] && $status == 'trialing') {
+                if ($tra['trial'] && $status == 'trialing') {
                     $status = 'trialing';
-                } elseif ($status == 'active' && $sub['subscription'][0]['status'] == 1 && !$sub['trial']) {
+                } elseif ($status == 'active' && $tra['subscription'][0]['status'] == 1 && !$tra['trial']) {
                     $nowDate = Carbon::now();
 
-                    if ($sub['ends_at']) {
-                        $sub_ends_at = Carbon::createFromFormat('Y-m-d H:i:s', $sub['ends_at']);
+                    if ($tra['ends_at']) {
+                        $sub_ends_at = Carbon::createFromFormat('Y-m-d H:i:s', $tra['ends_at']);
 
                         if ($sub_ends_at->gte($nowDate)) {
                             $status = 'active';
                         }
                     }
-                } elseif ($status == 'active' && $sub['subscription'][0]['status'] == 0 && !$sub['trial']) {
+                } elseif ($status == 'active' && $tra['subscription'][0]['status'] == 0 && !$tra['trial']) {
                     $status = 'paid_not_active_user_canceled';
-                } elseif (($status == 'cancelled' || $status == 'cancel' || $status == 'canceled') && !$sub['trial']) {
+                } elseif (($status == 'cancelled' || $status == 'cancel' || $status == 'canceled') && !$tra['trial']) {
                     $status = 'paid_and_cancelled';
-                } elseif (($status == 'cancelled' || $status == 'cancel' || $status == 'canceled') && $sub['trial']) {
+                } elseif (($status == 'cancelled' || $status == 'cancel' || $status == 'canceled') && $tra['trial']) {
                     $status = 'cancelled';
                 } else {
                     $status = '';
                 }
-                //dd($sub['user']);
+                //dd($tra['user']);
                 $registrationEvent = null;
-                foreach ($sub['user'][0]['events_for_user_list'] as $userEvent) {
+                foreach ($tra['user'][0]['events_for_user_list'] as $userEvent) {
                     $category = isset($userEvent['category'][0]['id']) ? $userEvent['category'][0]['id'] : -1;
                     if (in_array($category, $categories)) {
                         $registrationEvent = $userEvent;
                     }
                 }
 
-                $name = $sub['user'][0]['firstname'] . ' ' . $sub['user'][0]['lastname'];
-                $amount = '€' . number_format(intval($sub['total_amount']), 2, '.', '');
+                $name = $tra['user'][0]['firstname'] . ' ' . $tra['user'][0]['lastname'];
+                $amount = '€' . number_format(intval($tra['total_amount']), 2, '.', '');
 
                 $delivery = '-';
                 if ($registrationEvent) {
@@ -95,15 +97,15 @@ class SubscriptionController extends Controller
 
                 $total_users[$name] = $name;
 
-                $subscriptions[$sub['id']] = ['user_id' => $sub['user'][0]['id'], 'user' => $name, 'plan_id' => $plId, 'plan_name' => $sub['subscription'][0]['name'],
-                    'event_title' => $titleE, 'status' => $status, 'ends_at'=>$sub['ends_at'],
-                    'amount' => $amount, 'created_at'=>date('Y-m-d', strtotime($sub['created_at'])), 'id'=>$sub['id'],
+                $subscriptions[$tra['user'][0]['id']] = ['user_id' => $tra['user'][0]['id'], 'user' => $name, 'plan_id' => $plId, 'plan_name' => $tra['subscription'][0]['name'],
+                    'event_title' => $titleE, 'status' => $status, 'ends_at'=>$tra['ends_at'],
+                    'amount' => $amount, 'created_at'=>date('Y-m-d', strtotime($tra['created_at'])), 'id'=>$tra['id'],
                     'event_id' => $subEvId, 'delivery' => $delivery];
             }
         }
 
-        //dd($subscriptions);
-        $data['total_users'] = count($total_users);
+        // dd(count($subscriptions));
+        $data['total_users'] = count($subscriptions);
         $data['subscriptions'] = $subscriptions;
 
         return view('admin.subscription.subscription_list', $data);
