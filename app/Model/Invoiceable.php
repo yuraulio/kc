16cdbaabac41cdc73eb28c63cebfa692db083139
Lucas Muley <50708774+lucasmuleyv@users.noreply.github.com>
2024-03-01 10:43:11 +0100

    Feature/132 check the certifications for advanced digital marketing strategy course (#124)
    * feat: adjust image default sharing facebook
    
    * feat:Exams & Payment -> Not allow exam & certificate when installments not payed[60]
    
    * Apply php-cs-fixer changes
    
    ---------
    
    Co-authored-by: lucasmuleyv <lucasmuleyv@users.noreply.github.com>

--- app/Model/Invoiceable.php
+++ app/Model/Invoiceable.php
@@ -5,11 +5,7 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Transactionable extends Model
+class Invoiceable extends Model
 {
     use HasFactory;
-    protected $table = 'transactionables';
-
-    protected $fillable = [
-    ];
 }
