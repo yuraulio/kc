4c3d3dced6921dcbae42a9c154dd3e4813f3a797
korchevskiy <sergey.korchevskiy@gmail.com>
2024-03-04 11:04:03 +0200

    feat(Pages): use "blue" color for dates  [WEB-144] (#142)

--- resources/views/new_web/components/publish_date_line_component.blade.php
+++ resources/views/new_web/components/publish_date_line_component.blade.php
@@ -1,4 +1,4 @@
-<div class="heading-small text-muted mb-1 mt-1">
+<div class="heading-small text-primary mb-1 mt-1">
   @if(Carbon::parse($page->published_at)->startOfDay()->lt(Carbon::parse($page->updated_at)->startOfDay()))
   Published: {{ Carbon::parse($page->published_at)->format('d M Y') }},
   Last updated: {{ Carbon::parse($page->updated_at)->format('d M Y') }}
