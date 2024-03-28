ea72b9fb40cbc93a1b16582b9e4ac077b59ee1f8
FotiniKnowcrunch <163511633+FotiniKnowcrunch@users.noreply.github.com>
2024-03-28 10:26:40 +0200

    task #211 new admin link

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -50,9 +50,16 @@
                                     </a>
                                 </li>
 
-                                <li class="nav-item {{ isset($elementName) and $elementName  == 'dashboard' ? 'active' : '' }}">
-                                    <a href="{{ route('home') }}" class="nav-link">{{ __('Dashboard') }}</a>
-                                </li>
+                              <li class="nav-item active">
+                                <a class="nav-link active" href="https://admin-rose-eta.vercel.app/home/" target="_blank">
+                                  <span class="badge badge-primary">New</span> &nbsp
+                                  {{ __('React Admin') }}
+                                </a>
+                              </li>
+
+{{--                                <li class="nav-item {{ isset($elementName) and $elementName  == 'dashboard' ? 'active' : '' }}">--}}
+{{--                                    <a href="{{ route('home') }}" class="nav-link">{{ __('Dashboard') }}</a>--}}
+{{--                                </li>--}}
 
                                 <!-- <li class="nav-item {{ isset($elementName) and $elementName  == 'dashboard-alternative' ? 'active' : '' }}">
                                     <a href="{{ route('page.index','dashboard-alternative') }}" class="nav-link">{{ __('Alternative') }}</a>
