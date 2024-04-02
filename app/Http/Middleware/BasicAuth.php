6949fa688b8bdb18501c98f05d830219e3d5a9fe
Andrew Vaskovets <andrew.v@drupfan.com>
2024-04-02 13:46:01 +0300

    feat: add the temporary solution for BasicAuth
    I add this hack to unblock devs who use the API. It will be changed to the Nginx configuration later.

---
+++ app/Http/Middleware/BasicAuth.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * TODO: Important - this middleware is needed for dev environments delete it before deploying to the production.
+ */
+class BasicAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Allow making the requests to the API.
+        if ($request->wantsJson()) {
+            return $next($request);
+        }
+
+        // Check if BasicAuth is enabled.
+        if (!env('BASIC_AUTH', false)) {
+            return $next($request);
+        }
+
+        header('Cache-Control: no-cache, must-revalidate, max-age=0');
+        $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
+        $is_not_authenticated = (
+            !$has_supplied_credentials ||
+            $_SERVER['PHP_AUTH_USER'] != env('BASIC_AUTH_USER') ||
+            $_SERVER['PHP_AUTH_PW']   != env('BASIC_AUTH_PASSWORD')
+        );
+
+        if ($is_not_authenticated) {
+            header('HTTP/1.1 401 Authorization Required');
+            header('WWW-Authenticate: Basic realm="Access denied"');
+            exit;
+        }
+
+        return $next($request);
+    }
+}
