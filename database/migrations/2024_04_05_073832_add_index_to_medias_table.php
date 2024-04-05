1c4895f3f805dc6ac525bfb19d80cb2a08e411b8
Andrew Vaskovets <andrew.v@drupfan.com>
2024-04-05 08:15:00 +0300

    feat: add indexes to the medias and the event_user tables

---
+++ database/migrations/2024_04_05_073832_add_index_to_medias_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('medias', function (Blueprint $table) {
+            $table->index(['mediable_id', 'mediable_type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('medias', function (Blueprint $table) {
+            $table->dropIndex(['mediable_id', 'mediable_type']);
+        });
+    }
+};
---
+++ database/migrations/2024_04_05_081059_add_index_to_event_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('event_user', function (Blueprint $table) {
+            $table->index('event_id');
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('event_user', function (Blueprint $table) {
+            $table->dropIndex('event_id');
+            $table->dropIndex('user_id');
+        });
+    }
+};
