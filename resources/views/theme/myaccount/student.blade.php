ffa89b36c2b20bcacb3e00fae9d972d334a5b1a0
FotiniKnowcrunch <163511633+FotiniKnowcrunch@users.noreply.github.com>
2024-04-17 18:27:37 +0300

    exams fix for #c-exams-inner

--- resources/views/theme/myaccount/student.blade.php
+++ resources/views/theme/myaccount/student.blade.php
@@ -1323,9 +1323,16 @@
                                                     <li class="active"><a href="#c-watch-inner{{$tab}}">Watch</a></li>
 
 
-                                                    @if(isset($event['exams']) && count($event['exams']) >0 )
-                                                    <li><a href="#c-exams-inner{{$tab}}">Exams</a></li>
-                                                    @endif
+{{--                                                    @if(isset($event['exams']) && count($event['exams']) >0 )--}}
+{{--                                                    <li><a href="#c-exams-inner{{$tab}}">Exams</a></li>--}}
+{{--                                                    @endif--}}
+                                                  @if(isset($event['exams']) && count($event['exams']) > 0)
+                                                    <li>
+                                                      <button onclick="document.getElementById('c-exams-inner{{$tab}}').scrollIntoView({behavior: 'smooth'});">
+                                                        Exams
+                                                      </button>
+                                                    </li>
+                                                  @endif
                                                     @if(count($event['certs']) > 0)
                                                     <li><a href="#c-cert-inner{{$tab}}">Certificate</a></li>
                                                     @endif
@@ -1646,9 +1653,16 @@
                                                 <ul>
                                                     {{--<li class="active"><a href="#c-info-inner{{$tab}}">Info</a></li>--}}
                                                     <li class="active"><a href="#c-watch-inner{{$tab}}">Watch</a></li>
-                                                    @if(isset($event['exams']) && count($event['exams']) >0 )
-                                                    <li><a href="#c-exams-inner{{$tab}}">Exams</a></li>
-                                                    @endif
+{{--                                                    @if(isset($event['exams']) && count($event['exams']) >0 )--}}
+{{--                                                    <li><a href="#c-exams-inner{{$tab}}">Exams</a></li>--}}
+{{--                                                    @endif--}}
+                                                  @if(isset($event['exams']) && count($event['exams']) > 0)
+                                                    <li>
+                                                      <button onclick="document.getElementById('c-exams-inner{{$tab}}').scrollIntoView({behavior: 'smooth'});">
+                                                        Exams
+                                                      </button>
+                                                    </li>
+                                                  @endif
 
                                                     @if(count($event['certs']) > 0)
                                                     <li><a href="#c-cert-inner{{$tab}}">Certificate</a></li>
