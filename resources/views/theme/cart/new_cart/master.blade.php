4f3516c26c92521d758e0f79b2e4fcd86776d2b5
Fotini Aposkiti <fotini.aposkiti@gmail.com>
2024-05-30 15:41:03 +0300

    remove google analytics for dev environment

--- resources/views/theme/cart/new_cart/master.blade.php
+++ resources/views/theme/cart/new_cart/master.blade.php
@@ -18,12 +18,12 @@
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','GTM-ML7649C');
     </script>
-  @elseif(config('app.env') == "development")
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-MLLXRGTK');</script>
+{{--  @elseif(config('app.env') == "development")--}}
+{{--    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':--}}
+{{--      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],--}}
+{{--      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=--}}
+{{--      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);--}}
+{{--      })(window,document,'script','dataLayer','GTM-MLLXRGTK');</script>--}}
   @endif
 	<!-- End Google Tag Manager -->
 
