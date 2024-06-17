90151d5ce10cbe498a71dc72dd8cc07f7ca9901e
Andrew Vaskovets <andrew.v@drupfan.com>
2024-06-17 09:25:05 +0300

    feat: change query of getting lessons for the topic to remove duplicates

--- app/Model/Topic.php
+++ app/Model/Topic.php
@@ -7,6 +7,8 @@ use App\Model\Event;
 use App\Model\Lesson;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Topic extends Model
 {
@@ -17,38 +19,48 @@ class Topic extends Model
         'priority', 'status', 'comment_status', 'title', 'short_title', 'subtitle', 'header', 'summary', 'body', 'author_id', 'creator_id', 'email_template',
     ];
 
-    public function category()
+    public function category(): MorphToMany
     {
         return $this->morphToMany(Category::class, 'categoryable');
     }
 
-    public function topic()
+    public function topic(): MorphToMany
     {
         return $this->morphToMany(Category::class, 'categoryable');
     }
 
-    public function category1()
+    public function category1(): BelongsToMany
     {
         return $this->belongsToMany(Category::class, 'categories_topics_lesson', 'category_id', 'topic_id');
     }
 
-    public function lessonsCategory()
+    public function lessonsCategory(): BelongsToMany
     {
-        return $this->belongsToMany(Lesson::class, 'categories_topics_lesson')->withPivot('category_id', 'priority');
+        return $this->belongsToMany(Lesson::class, 'categories_topics_lesson')
+            ->withPivot('category_id', 'priority');
     }
 
-    public function lessons()
+    public function lessons(): BelongsToMany
     {
-        return $this->belongsToMany(Lesson::class, 'event_topic_lesson_instructor')->withPivot('event_topic_lesson_instructor.priority')->with('type')->orderBy('event_topic_lesson_instructor.priority', 'asc');
+        return $this->belongsToMany(Lesson::class, 'event_topic_lesson_instructor')
+            ->withPivot('event_topic_lesson_instructor.priority')
+            ->with('type')
+            ->orderBy('event_topic_lesson_instructor.priority')
+            ->groupBy('id');
     }
 
-    public function event_topic()
+    public function event_topic(): BelongsToMany
     {
-        return $this->belongsToMany(Topic::class, 'event_topic_lesson_instructor')->withPivot('event_id', 'lesson_id', 'instructor_id', 'date', 'priority', 'time_starts', 'time_ends', 'duration', 'room');
+        return $this->belongsToMany(Topic::class, 'event_topic_lesson_instructor')
+            ->withPivot('event_id', 'lesson_id', 'instructor_id', 'date', 'priority', 'time_starts', 'time_ends', 'duration', 'room');
     }
 
-    public function event_lesson()
+    public function event_lesson(): BelongsToMany
     {
-        return $this->belongsToMany(Lesson::class, 'event_topic_lesson_instructor')->select('lessons.*', 'event_id')->where('status', true)->withPivot('event_id', 'lesson_id', 'instructor_id', 'date', 'priority', 'time_starts', 'time_ends', 'duration', 'room')->with('instructor');
+        return $this->belongsToMany(Lesson::class, 'event_topic_lesson_instructor')
+            ->select('lessons.*', 'event_id')
+            ->where('status', true)
+            ->withPivot('event_id', 'lesson_id', 'instructor_id', 'date', 'priority', 'time_starts', 'time_ends', 'duration', 'room')
+            ->with('instructor');
     }
 }
