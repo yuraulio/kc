677b3453b0cbd0fa372bac7430af2edca3b74203
AndrewVaskovets <AndrewVaskovets@users.noreply.github.com>
2024-07-18 09:16:11 +0000

    Apply php-cs-fixer changes

--- app/Model/EventTicket.php
+++ app/Model/EventTicket.php
@@ -6,12 +6,10 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class EventTicket extends Pivot
 {
-
     protected $table = 'event_tickets';
 
     protected $casts = [
         'options' => 'array',
         'features' => 'array',
     ];
-
 }
