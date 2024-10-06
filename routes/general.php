990661a0582a626aa332d04a096af9686edc3bbd
max.s@drupfan.com <max.s@drupfan.com>
2024-10-06 13:07:01 +0200

    Fix general domain

--- routes/general.php
+++ routes/general.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\New_web\MainController;
 
 Route::pattern('domain', '(' . implode('|', explode(',', config('app.app_domain'))) . ')');
-Route::group(['domain' => config('app.prefix_new_admin').'{domain}'], function () {
+Route::group(['domain' => '{domain}'], function () {
     Route::get('/__preview/{uuid}', [DashboardController::class, 'page']);
 
     Route::get('/', [MainController::class, 'index'])->name('homepage');
